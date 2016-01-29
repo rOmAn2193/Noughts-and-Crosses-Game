d073ce643e545e249dae8bafcc51c19301a3bbd5
rOmAn2193 <roman_puzey@aol.com>
2016-01-28 23:22:52 -0700

    Initial Commit

---
+++ Noughts and CrossesTests/Noughts_and_CrossesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Noughts_and_CrossesTests.swift
+//  Noughts and CrossesTests
+//
+//  Created by Roman on 1/28/16.
+//  Copyright © 2016 Roman Puzey. All rights reserved.
+//
+
+import XCTest
+@testable import Noughts_and_Crosses
+
+class Noughts_and_CrossesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
